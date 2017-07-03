a15f64d18ede4f83f524754049c0c31758e5c4dc
Roland Tepp <rolandt@PC0361.local>
2017-07-03 09:07:43 +0300

    Refactor common logic for reading XML from stdin or list of filenames depending on CLI arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,66 @@
 use std::io::{Read, Write};
 use std::fs::File;
 use std::path::Path;
+use std::iter::Iterator;
 
 use std::fmt::{Display, Formatter};
 
 #[macro_use]
 extern crate clap;
-use clap::{App, Values, ArgMatches};
+use clap::{Values, ArgMatches};
 
 extern crate xml;
-use xml::reader::{EventReader, XmlEvent};
+use xml::reader::EventReader;
 
 enum InputSource<'a> {
     StdInput,
     Filename(&'a str)
 }
 
-impl <'a> InputSource<'a> {
+impl<'a> InputSource<'a> {
     fn from_path(path: &'a str) -> InputSource<'a> {
         InputSource::Filename(path)
     }
     fn from_stdin() -> InputSource<'a> {
         InputSource::StdInput
     }
+    fn open(&self) -> std::io::Result<Box<Read>> {
+        match *self {
+            InputSource::StdInput => {
+                let read : Box<Read> = Box::new(std::io::stdin());
+                Ok(read)
+            },
+            InputSource::Filename(path) => {
+                let path = Path::new(path);
+                match File::open(path) {
+                    Ok(file) => Ok(Box::new(file)),
+                    Err(e) => Err(e)
+                }
+            }
+        }
+    }
 }
 
-impl <'a> Display for InputSource<'a> {
+impl<'a> Display for InputSource<'a> {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match *self {
-            InputSource::StdInput => write!(f, "std::io::stdout()"),
-            InputSource::Filename(path) => path.display().fmt(f)
+            InputSource::StdInput => write!(f, "std::io::stdin()"),
+            InputSource::Filename(path) => write!(f, "{}", path)
         }
     }
 }
 
-
-fn input_sources(values: Option<Values>)  {
-    match values {
-        None => {
-            std::iter::once(InputSource::from_stdin()).enumerate();
-        },
-        Some(v) => {
-            let v: Values = v;
-            if v.is_empty() {
-                std::iter::once(InputSource::from_stdin()).enumerate();
-            }
-            v.map(Path::new).map(InputSource::from_path).enumerate();
+fn input_sources(values: Option<Values>) -> Vec<InputSource> {
+    if let Some(values) = values {
+        let values : Vec<InputSource> = values.map(InputSource::from_path).collect();
+        if !values.is_empty() {
+            return values;
         }
     }
+    return vec!(InputSource::from_stdin());
 }
+
+
 
 fn main() {
     
@@ -68,10 +80,22 @@ fn main() {
     }
 
     let inputs = input_sources(m.values_of("FILES"));
-
     for input in inputs {
-        let input: InputSource = input;
+        let input = Box::new(input);
         println!("Reading from {}", input);
 
+        match input.open() {
+            Ok(source) => {
+                let events = EventReader::new(source);
+                for event in events {
+                    writeln!(std::io::stdout(), "XML Event: {:?}", event).unwrap();
+                }
+            }
+            Err(e) => {
+                writeln!(std::io::stderr(), "Could not open {}: \n {}", input, e).unwrap();
+            }
+        }
     }
+
+
 }
