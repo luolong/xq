95044e2c6ee5ffaf8932c6c23e3a4bc0cab3b0e0
Roland Tepp <roland@tepp.info>
2017-07-07 18:16:28 +0300

    Added rudimentary Rustdoc comments

--- src/xq/input_source.rs
+++ src/xq/input_source.rs
@@ -3,22 +3,30 @@ use std::fs::File;
 use std::path::Path;
 use std::fmt::{self, Display, Formatter};
 
-/// Input source
+/// Source of input
 ///
+/// Command line application accepts input from two sources:
+/// - files
+/// - standard input
 ///
+/// Input sources can be _opened_ for reading.
 pub enum InputSource<'a> {
     StdInput,
     Filename(&'a str)
 }
 
 impl<'a> InputSource<'a> {
+    /// Create an input source from a filename
     pub fn from_path(path: &'a str) -> InputSource<'a> {
         InputSource::Filename(path)
     }
+
+    /// Create an input source from standard input
     pub fn from_stdin() -> InputSource<'a> {
         InputSource::StdInput
     }
 
+    /// Open input source for reading
     pub fn open(&self) -> io::Result<Box<Read>> {
         match *self {
             InputSource::StdInput => {
